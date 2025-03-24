a1aabe67cbcb8975c9105ade99e7f864ee882efa
Alex <alexsr2504@gmail.com>
2025-03-24 12:42:14 +0000

    acabar titutlar

---
+++ A06/cercaPerTitular.php
@@ -0,0 +1,37 @@
+<?php 
+    $db = new SQLite3('diariLocal.db');
+
+    if (isset($_GET['nom'])) {
+        $introduit = $_GET['nom'];
+        
+        $stmt = $db->prepare("SELECT * FROM noticies WHERE LOWER(not_titular) = LOWER(:titular)");
+        $stmt->bindValue(':titular', $introduit, SQLITE3_TEXT);
+        $resultats = $stmt->execute();
+
+        $foundResults = false;
+        while ($fila = $resultats->fetchArray(SQLITE3_ASSOC)) {
+            if (!$foundResults) {
+                $foundResults = true;
+            }
+            echo "ID: ". $fila['not_id'] . "<br>";
+            echo "Titular: ". $fila['not_titular'] . "<br>";
+            echo "Cos: ". $fila['not_cos'] . "<br>";
+            echo "Data: ". $fila['not_data'] . "<br>";
+            echo "Secció: ". $fila['not_seccio'] . "<br><br>";
+        }
+        echo "<br>";
+        if (!$foundResults) {
+            echo "No hi ha resultats.";
+        }    
+        echo '<form action="cercaPerTitular.php"><input type="submit" value="Torna a la pagina de cerca"></form>';
+        
+    } else {
+        echo '<form action="cercaPerTitular.php" method="get">
+                <label for="nom">Titular exacte per cercar: </label>
+                <input type="text" name="nom" id="nom" required placeholder="Escriu el titular a cercar"><br>
+                <input type="submit" value="Cerca">
+              </form>';
+    }
+    
+    $db->close();
+?>
