bc084a02740f518733adbbd717d6cde5bd1e77da
Alex <alexsr2504@gmail.com>
2025-05-05 11:46:53 +0000

    po

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
-    echo "<a href='creacioApi'>M7-A15 - Creació d’APIs I  👉 👈</a>";
+    echo "<a href='creacioApi'>M7-A15 - Creació d’APIs I  👉 👈</a> <br><br>";
+    echo "<a href='a20'>M7-A20 - Usant MVC 🍷🍺</a>";
 ?>
